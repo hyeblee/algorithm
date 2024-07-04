e98691222ef01b56fc3741289bfc840dbb2ee1d1
hyeblee <hyeb1224@naver.com>
2024-07-04 23:31:10 +0900

    2024-07-04

---
+++ 프로그래머스 코딩테스트 입문/2024-07-01/src/Day1/day1_03.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class day1_03 {
+	public int solution(int num1, int num2) {
+        return num1*num2;
+    }
+}
---
+++ 프로그래머스 코딩테스트 입문/2024-07-03/src/Day3/day3_01.java
@@ -0,0 +1,9 @@
+package Day3;
+
+public class day3_01 {
+	public int solution(int num1, int num2) {
+        int answer = num1 % num2;
+        
+        return answer;
+    }
+}
