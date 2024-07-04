f11449872de0c037f108dacedeb503236b7f22f7
hyeblee <hyeb1224@naver.com>
2024-07-04 23:16:49 +0900

    2024-07-03(2)

--- 2024-07-03/src/Day3/day3_04.java
+++ 2024-07-03/src/Day3/day3_04.java
@@ -1,5 +1,22 @@
 package Day3;
 
-public class day3_04 {
+import java.util.Arrays;
 
+public class day3_04 {
+	public int[] solution(int n) {
+		int[] answer = new int[(n+1)/2];
+		if(n%2 == 0) 
+			n -= 1;
+		
+		for(int i = 1; i<= n; i++) {
+			answer[i/2] = i;
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		day3_04 my = new day3_04();
+		int[] result = my.solution(10);
+		System.out.println(Arrays.toString(result));
+	}
 }
