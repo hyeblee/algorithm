c8f7cbd6f696ed11c5f9cdd14142e6713308f5eb
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-07-13 19:52:20 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,16 +1,16 @@
-import java.util.*;
-
 class Solution {
     public String solution(int age) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         
+        StringBuilder sb = new StringBuilder();
         while(age>0){
-            int n = age % 10;
-            sb.append((char)('a'+n));
-            System.out.println(sb);
+            int n = age%10;
+            sb.append((char)('a' + n));
             age/=10;
         }
-        String answer = sb.reverse().toString();
+        
+        answer = sb.reverse().toString();
+        
         return answer;
     }
 }
