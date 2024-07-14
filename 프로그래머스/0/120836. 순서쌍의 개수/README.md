f48bb11ebe515724fd538697fbbed520c0f10e3b
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-07-14 18:47:48 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.07 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/README.md
+++ 프로그래머스/0/120836. 순서쌍의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 순서쌍의 개수 - 120836 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120836) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120836#) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.65 ms
+메모리: 74.2 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 14일 18:35:15
+2024년 07월 14일 18:47:47
 
 ### 문제 설명
 
--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -3,18 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        HashMap <Integer, Integer> map = new HashMap<>();
-        double squareRoot = Math.sqrt(n);
-        for(int i = 1; i< squareRoot; i++){
-            if(n % i == 0){
-                map.put(i, n/i);
-                answer++;
-            }
+        
+        for(int i = 1; i <= Math.sqrt(n); i++){
+            if(n % i == 0)
+                answer+=2;
+
+            if(i == Math.sqrt(n))
+                answer-=1;
         }
-        answer *= 2;
-        System.out.println(map);
-        if(squareRoot % 1 == 0)
-            answer+=1;
         return answer;
     }
 }
