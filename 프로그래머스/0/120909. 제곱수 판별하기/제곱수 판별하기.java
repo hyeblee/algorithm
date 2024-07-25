99fecc5a936a6189259bca3b114fc3b477dda9ab
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-07-25 23:06:19 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+
+        double sqrt = Math.sqrt(n);
+        if((int)(sqrt) == sqrt)
+            return 1;
+        else
+            return 2;
+        
+     
+    }
+}
