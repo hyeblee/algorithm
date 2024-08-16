0c9c1bfcbde2d418541514d7c8fa59dd5c2c568a
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-08-16 23:11:46 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.05 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        answer = my_string.repeat(k);
+        return answer;
+    }
+}
