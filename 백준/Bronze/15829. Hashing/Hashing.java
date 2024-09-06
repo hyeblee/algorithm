8ca191a6c4852ca0332ee88ce214cec9e1759386
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-09-06 14:15:34 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17696 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,19 +6,26 @@ public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		
 		int L = sc.nextInt();
 		String str = sc.next();
 		sc.close();
 		
-		int result = 0;
+		long result = 0;
+		long power = 1;
 		
 		for(int i=0;i<str.length();i++) {
 			int n = str.charAt(i) - 'a' + 1;
-			result += (n*Math.pow(31, i))%1234567891;
+			
+			result += (n*power);
+			power = (power*31)%1234567891;
+			
+			
+			
 		}
 		
 		
-		System.out.println(result);
+		System.out.println(result%1234567891);
 		
 	}
 }
