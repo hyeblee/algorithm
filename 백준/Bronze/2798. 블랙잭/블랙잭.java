147d9ca43fe8d3ca0bef1d6b1e0be11e568366f8
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-09-06 09:55:54 +0900

    [Bronze II] Title: 블랙잭, Time: 192 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main{
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		int result = 0;
+		for(int i=0;i<N;i++) {
+			int total = 0;
+			for(int j=i+1;j<N;j++) {
+				for(int k=j+1;k<N;k++) {
+					total = arr[i] + arr[j] + arr[k];
+					if(total > M)
+						continue;
+					if(Math.abs(M-total)<Math.abs(M-result))
+						result = total;
+				}
+			}
+		}
+		
+		
+		
+		System.out.println(result);
+		
+	}
+}
