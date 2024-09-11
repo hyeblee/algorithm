ca16748bd7bfc16c8357e313bb39cda1d0e73ee9
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-09-11 11:55:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1872 ms, Memory: 185732 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Dot{
+	int x;
+	int y;
+}
+
+class myComparator implements Comparator<Dot>{
+	public int compare(Dot d1, Dot d2) {
+		if(d1.x==d2.x)
+			return Integer.compare(d1.y, d2.y);
+		return Integer.compare(d1.x, d2.x);
+	}
+}
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		Dot[] dots = new Dot[n];
+		for(int i=0;i<n;i++) {
+			dots[i] = new Dot();
+			dots[i].x = sc.nextInt();
+			dots[i].y = sc.nextInt();
+		}
+		
+		Arrays.sort(dots, new myComparator());
+		for(Dot dot : dots) {
+			System.out.println(dot.x + " " + dot.y);
+		}
+		
+	}
+}
