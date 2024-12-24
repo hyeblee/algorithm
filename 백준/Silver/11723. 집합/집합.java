54daedce17d08eb99fcb0396351bfc3f64577100
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-12-24 16:37:13 +0900

    [Silver V] Title: 집합, Time: 992 ms, Memory: 312760 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,88 +1,47 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Deque;
-import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static int M;
-	public static Deque<Integer> s = new ArrayDeque<>();
-	public static List<Integer> arr = new ArrayList<>();
 
-	public static void add(Deque s, int x) {
-		if (check(s, x))
-			return;
-		s.addLast(x);
-	}
-
-	public static void remove(Deque s, int x) {
-		if (!check(s, x))
-			return;
-		s.remove(x);
-	}
-
-	public static boolean check(Deque s, int x) {
-		if (s.contains(x))
-			return true;
-		return false;
-	}
-
-	public static void toggle(Deque S, int x) {
-		if (check(s, x)) {
-			s.remove(x);
-			return;
-		}
-		s.add(x);
-	}
-
-	public static void all(Deque S) {
-		s.clear();
-		s.addAll(arr);
-	}
-
-	public static void empty(Deque S) {
-		s.clear();
-	}
-
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+		StringBuilder sb = new StringBuilder("");
+		int s = 0;
 		M = Integer.parseInt(br.readLine());
-		for (int i = 0; i < 20; i++) {
-			arr.add(i + 1);
-		}
-
-		for (int i = 0; i < M; i++) {
-			String[] input = br.readLine().split(" ");
-			String cmd = input[0];
-			if (cmd.equals("add")) {
-				int x = Integer.parseInt(input[1]);
-				add(s, x);
-			} else if (cmd.equals("remove")) {
-				int x = Integer.parseInt(input[1]);
-				remove(s, x);
-			} else if (cmd.equals("check")) {
-				int x = Integer.parseInt(input[1]);
-				int result = check(s, x) ? 1 : 0;
-				bw.write(result+"\n");
-			} else if (cmd.equals("toggle")) {
-				int x = Integer.parseInt(input[1]);
-				toggle(s, x);
-			} else if (cmd.equals("all")) {
-				all(s);
-			} else if (cmd.equals("empty")) {
-				empty(s);
+		for(int i=0;i<M;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			String cmd = st.nextToken();
+			if(cmd.equals("add")) {
+				int x = Integer.parseInt(st.nextToken());
+				if(((s>>x)&1)==0)
+					s += 1<<x;
+			}
+			else if(cmd.equals("remove")) {
+				int x = Integer.parseInt(st.nextToken());
+				if(((s>>x)&1)!=0)
+					s-=1<<x;
+			}
+			else if(cmd.equals("check")) {
+				int x = Integer.parseInt(st.nextToken());
+				if(((s>>x)&1)!=0)
+					sb.append(1+"\n");
+				else
+					sb.append(0+"\n");
+			}
+			else if(cmd.equals("toggle")) {
+				int x = Integer.parseInt(st.nextToken());
+				s ^= (1<<x);
+			}
+			else if(cmd.equals("all")) {
+				s = (1<<21)-1;
+			}
+			else if(cmd.equals("empty")) {
+				s = 0;
 			}
 		}
-		bw.flush();
-		br.close();
-		bw.close();
-
+		System.out.println(sb);
 	}
 }
