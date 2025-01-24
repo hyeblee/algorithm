2a416d32e6d5328b97e75239efdce15801c4528a
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-01-24 12:46:12 +0900

    [Silver II] Title: 스택 수열, Time: 272 ms, Memory: 26044 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,42 +1,43 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+// 1~n의 숫자를 오름차순으로 push하는데, 이 스택을 중간에 pop하여 입력받은 수열 만들기
+
+
+
+public class Main {
+	public static int n;
+	public static Deque<Integer> stack = new ArrayDeque<>();
+	public static StringBuilder sb = new StringBuilder("");
+	public static int element = 1;
+	
+	public static boolean makeSequence(int num) {
+		// n이 아직 스택에 없다면, element 키우면서 스택에 넣어주기
+		while(element <= num) {
+			stack.push(element++);
+			sb.append("+\n");
+		}
+		while(!stack.isEmpty()) {
+			sb.append("-\n");
+			if(stack.pop()==num) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
 		for(int i=0;i<n;i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Deque<Integer> stack = new LinkedList<>();
-		StringBuilder sb = new StringBuilder("");
-		int idx=0;
-		for(int i=1;i<=n;i++) {
-			
-			if(i==arr[idx]) {
-				sb.append("+\n-\n");
-				idx++;
-			}
-			else {
-				stack.push(i);
-				sb.append("+\n");
-			}
-			while(stack.size()>0&&arr[idx]==stack.peekFirst()) {
-				stack.pollFirst();
-				sb.append("-\n");
-				idx++;
+			int num = Integer.parseInt(br.readLine());
+			if(!makeSequence(num)) {
+				System.out.println("NO");
+				return;
 			}
 		}
-
-		if(idx==n)
-			System.out.println(sb);
-		else
-			System.out.println("NO");
 		
-		
+		System.out.println(sb);
 	}
 }
