9e1e12fd09720b214b8e0f73266b83c87c1e4caa
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-01-31 16:26:43 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 256 ms, Memory: 48804 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,74 +1,73 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
-    private static final int[] dx = {0, 0, 1, -1};
-    private static final int[] dy = {1, -1, 0, 0};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] NM = br.readLine().split(" ");
-        int n = Integer.parseInt(NM[0]);
-        int m = Integer.parseInt(NM[1]);
-
-        String[][] campus = new String[n][m];
-
-        int targetX = 0;
-        int targetY = 0;
-
-        for (int i = 0; i < n; i++) {
-            String[] line = br.readLine().split("");
-            for (int j = 0; j < m; j++) {
-                campus[i][j] = line[j];
-                if (campus[i][j].equals("I")) {
-                    targetX = i;
-                    targetY = j;
-                }
-            }
-        }
-
-        System.out.println(bfs(campus, n, m, targetX, targetY));
-
-        br.close();
-    }
-
-    private static String bfs(String[][] maze, int n, int m, int targetX, int targetY) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{targetX,targetY});
-
-        boolean[][] isVisited = new boolean[n][m];
-        isVisited[targetX][targetY] = true;
-
-        int res = 0;
-
-        while (!queue.isEmpty()) {
-            int[] step = queue.poll();
-            int x = step[0];
-            int y = step[1];
-
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || maze[nx][ny].equals("X")) {
-                    continue;
-                }
-
-                if(isVisited[nx][ny])
-                    continue;
-
-                if (maze[nx][ny].equals("P")) {
-                    res++;
-                }
-
-                queue.offer(new int[]{nx, ny});
-                isVisited[nx][ny] = true;
-            }
-        }
-
-        if (res == 0) {
-            return "TT";
-        } else return String.valueOf(res);
-    }
+	public static int N,M;
+	public static char[][] grid;
+	public static boolean[][] visit;
+	public static int cnt = 0;
+	public static int[] dx = {0,0,-1,1};
+	public static int[] dy = {-1,1,0,0};
+	
+	public static boolean isValid(int x, int y) {
+		return (x>=0&&x<M&&y>=0&&y<N);
+	}
+	public static void dfs(int curX, int curY) {
+		if(grid[curY][curX]=='P')
+			cnt++;
+		visit[curY][curX] = true;
+		for(int i=0;i<4;i++) {
+			int x = curX + dx[i];
+			int y = curY + dy[i];
+			if(!isValid(x,y))
+				continue;
+			if(!visit[y][x]&&grid[y][x]!='X')
+				dfs(x,y);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		grid = new char[N][M];
+		visit = new boolean[N][M];
+		int curX=0,curY=0;
+		
+		for(int i=0;i<N;i++) {
+			String str = br.readLine();
+			for(int j=0;j<M;j++) {
+				grid[i][j] = str.charAt(j);
+				if(grid[i][j]=='I') {
+					curX = j;
+					curY = i;
+				}
+			}
+		}
+		dfs(curX,curY);
+		if(cnt==0)
+			System.out.println("TT");
+		else		
+			System.out.println(cnt);
+	}
 }
