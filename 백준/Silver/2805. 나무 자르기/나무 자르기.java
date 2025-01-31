df4f7e6f827b81cd2d57a5fdf130788f388a5da4
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-01-31 10:28:44 +0900

    [Silver II] Title: 나무 자르기, Time: 916 ms, Memory: 119384 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -18,50 +18,48 @@ import java.util.Queue;
 
 public class Main {
 	public static int N, M;
-	public static List<Integer> trees = new ArrayList<>();
+	public static int[] trees;
 	
-	public static long getSum(long height) {
+	public static long getSum(int height) {
 		long sum = 0;
 		for(int tree: trees) {
-			if(tree>height)
-				sum += tree - height;
-			
+			if(tree > height)
+				sum += tree-height;
 		}
 		return sum;
 	}
 	
-	public static long binarySearch(long left, long right, long target) {
+	public static int binarySearch(int target) {
+		int left = 0;
+		int right = trees[N-1];
 		
 		while(left <= right) {
-			long mid = (right+left)/2;
-			long sum = getSum(mid);
-			if(sum>=M)
-				left = mid+1;
-			else
-				right = mid-1;
-			
+			int mid = (int)((long)left + (long)right)/2;
+			long result = getSum(mid);
+			if(result >= target) {
+				left = mid + 1;
+			}
+			else // 나무 합이 목표가 안되면 절단기 높이 줄이기
+				right = mid - 1;
 		}
+		
 		return right;
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		
+		trees = new int[N];
 		st = new StringTokenizer(br.readLine());
-		while(st.hasMoreElements()) {
-			int n = Integer.parseInt(st.nextToken());
-			trees.add(n);
-		}
-		Collections.sort(trees);
 		
-		br.close();
-		long max = trees.get(N-1);
-		long result = binarySearch(0,max,M);
-		System.out.println(result);
+		for(int i=0;i<N;i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(trees);
+		System.out.println(binarySearch(M));
+		
 	}
 }
