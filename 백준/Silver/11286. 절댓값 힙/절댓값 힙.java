3ec81ba1ce0eb6e11cf819ed18912796538430a0
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-02-21 10:58:56 +0900

    [Silver I] Title: 절댓값 힙, Time: 316 ms, Memory: 26068 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,30 +1,56 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-            int abs1 = Math.abs(o1);
-            int abs2 = Math.abs(o2);
-
-            if(abs1 == abs2) return o1 > o2 ? 1 : -1;
-            return abs1 - abs2;
-        });
-
-        for(int i = 0 ; i < n; i++){
-            int val = Integer.parseInt(br.readLine());
-            if(val == 0){
-                if(queue.isEmpty()) System.out.println("0");
-                else System.out.println(queue.poll());
-            }else{
-                queue.add(val);
-            }
-        }
-    }
+	public static class myComparator implements Comparator<Integer>{
+		@Override
+		public int compare(Integer a, Integer b) {
+			if(Math.abs(a)==Math.abs(b))
+				return Integer.compare(a, b);
+			return Integer.compare(Math.abs(a), Math.abs(b));
+		}
+	}
+	
+	
+	
+	public static PriorityQueue pq = new PriorityQueue<>(new myComparator());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x==0) {
+				if(pq.isEmpty())
+					bw.write("0\n");
+				else
+					bw.write(pq.poll()+"\n");
+				continue;	
+			}
+			pq.offer(x);
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+	}
 }
