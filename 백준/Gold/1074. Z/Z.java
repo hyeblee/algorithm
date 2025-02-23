f9b01f03a8914d703836a8849b913b4d74667d8c
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-02-23 15:27:54 +0900

    [Gold V] Title: Z, Time: 172 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+public class Main {
+
+	static int N,r,c,cnt=0;
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		r = sc.nextInt();
+		c = sc.nextInt();
+		
+		find(r,c,(int)Math.pow(2, N));
+		System.out.println(cnt);
+		
+	
+	}
+	
+	static void find(int x,int y, int size) {
+		if(size==1)
+			return;
+		
+		if(x<size/2&&y<size/2) {	//왼쪽 위
+			find(x,y,size/2);
+		}else if(x<size/2&&size/2<=y) {	//오른쪽 위
+			cnt+=(size*size/4);
+			find(x,y-size/2,size/2);
+		}else if(x>=size/2&&size/2>y) { //왼쪽 아래
+			cnt+=(size*size/4)*2;
+			find(x-size/2,y,size/2);
+		}else {						//오른쪽 아래
+			cnt+=(size*size/4)*3;
+			find(x-size/2,y-size/2,size/2);
+			
+		}
+			
+	}
+	
+}
