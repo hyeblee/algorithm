90a1f915145b3260b07d42c245602b4572121796
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-03-28 23:14:35 +0900

    [Gold V] Title: A와 B 2, Time: 232 ms, Memory: 21376 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -13,25 +13,28 @@ public class Main {
 
 	public static String operation2(String str) { // 끝에 B 붙이고 뒤집는 연산
 		String result = "B";
-		for(int i=str.length()-1;i>=0;i--) {
-			result+=str.charAt(i);
+		for (int i = str.length() - 1; i >= 0; i--) {
+			result += str.charAt(i);
 		}
 		return new String(result);
 	}
 
 	public static void operations(String str, int n) {
-		
 
-		if (n == cnt ||( !T.contains(str)&& !T.contains(new StringBuilder(str).reverse()))) { // 연산 n번 수행하면 재귀 종료
+		if (n == cnt) { // 연산 n번 수행하면 재귀 종료
+			return;
+		}
+		
+		// 시간초과가 나서 이렇게 처리했는데 더 예븐 접근이 있을가요. . .
+		if (!T.contains(str) && !T.contains(new StringBuilder(str).reverse())) { // T가 str 포함하지 않는다면(str 뒤집은 것도) 재귀 종료
 			return;
 		} else {
 			String str1 = operation1(str);
 			String str2 = operation2(str);
-//			System.out.println(str1+": "+str2);
-			
+
 			operations(str1, n + 1);
 			operations(str2, n + 1);
-			if (str1.equals(T)||str2.equals(T)) { // S를 T로 만들 수 있으면 결과 1
+			if (str1.equals(T) || str2.equals(T)) { // S를 T로 만들 수 있으면 결과 1
 				result = 1;
 				return;
 			}
