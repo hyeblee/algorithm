2885c98f69c6e6cd4a8181f2bd3c08bf37fe29e0
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-01 15:54:39 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10924 ms, Memory: 308488 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+//노드의 갯수가 많을수록 bfs가 적합하다.
+public class Main {
+	public static int n, m;
+	public static int max = 0;
+	public static List<Integer>[] adj;
+	public static boolean[] visited;
+	public static int[] result;
+
+	public static void bfs(int start) {
+		visited[start] = true;
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.offer(start);
+		int cnt = 0;
+		while (!queue.isEmpty()) {
+			int cur = queue.poll();
+			cnt++;
+			for(int val: adj[cur]) {
+				if(!visited[val]) {
+					queue.offer(val);
+					visited[val] = true;
+					result[val]++;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		adj = new ArrayList[n+1];
+		result = new int[n+1];
+		for(int i=1;i<=n;i++) {
+			adj[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adj[a].add(b);
+		}
+
+		for (int i = 1; i <= n; i++) {
+			visited = new boolean[n+1];
+			bfs(i);
+		}
+		int max = 0;
+		for (int val : result) {
+			max = Integer.max(val, max);
+		}
+		for(int i=1;i<=n;i++) {
+			if(result[i]==max)
+				System.out.print(i+" ");
+		}
+
+	}
+}
