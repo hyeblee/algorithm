b399b67701e20ad67ccdcce3ef7f956f88cc6c45
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-16 17:15:18 +0900

    [Gold V] Title: 회의실 배정, Time: 492 ms, Memory: 45020 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,58 +1,55 @@
-import java.util.Scanner;
-import java.util.Set;
-import java.util.Stack;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
-	public static int n;
-	public static int[][] meetings;
-	
-	public static class myComparator implements Comparator<int[]>{
-		@Override
-		public int compare(int[] a, int[] b) {
-			if(a[1]==b[1]) { // 끝나는 시간이 같은 짧은 회의 기준으로 정렬
-				return Integer.compare(a[0], b[0]);
-			}
-			return Integer.compare(a[1], b[1]);
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		int cnt = 0;
-		meetings = new int[n][2];
-		for(int i=0;i<n;i++) {
-			meetings[i][0] = sc.nextInt();
-			meetings[i][1] = sc.nextInt();
-		}
-		
-		Arrays.sort(meetings,new myComparator());
-		int before = 0;
-		for(int i=0;i<n;i++) {
-			if(before<=meetings[i][0]) {
-				cnt++;
-				before = meetings[i][1];
-			}
-		}
-		System.out.println(cnt);
-	}
-	
+
+  public static int N;
+  public static Meeting[] meetings;
+
+  public static class Meeting implements Comparable<Meeting> {
+
+    int s;
+    int e;
+
+    public Meeting(int s, int e) {
+      this.s = s;
+      this.e = e;
+    }
+
+    @Override
+    public int compareTo(Meeting o) { // 끝나는 시간이 빠른 순(작은 순)으로 정렬
+      // 만약 끝나는 시간이 같다면, 시작하는 시간이 빠른순으로 정렬,,
+      if (this.e == o.e) {
+        return this.s - o.s;
+      }
+      return this.e - o.e;
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+
+    meetings = new Meeting[N];
+    for (int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int s = Integer.parseInt(st.nextToken());
+      int e = Integer.parseInt(st.nextToken());
+      meetings[i] = new Meeting(s, e);
+    }
+    Arrays.sort(meetings);
+    int count = 1;
+    Meeting cur = meetings[0];
+    for (int i = 1; i < N; i++) {
+      if (meetings[i].s >= cur.e) {
+        cur = meetings[i];
+        count++;
+      }
+    }
+
+    System.out.println(count);
+
+  }
 }
