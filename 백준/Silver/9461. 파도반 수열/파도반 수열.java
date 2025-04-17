7f76891b8c361fa75eea3d1ab079b10126340801
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-17 11:26:43 +0900

    [Silver III] Title: 파도반 수열, Time: 184 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,31 +1,25 @@
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
-	public static final int MAX_N = 100;
-	public static int T;
-	public static long[] result = new long[MAX_N+1];
-	
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		T = sc.nextInt();
-		result[1] = result[2] = result[3] = 1;
-		
-		for(int i=4;i<=100;i++) {
-			result[i] = result[i-3]+result[i-2];
-		}
-		
-		for(int i=0;i<T;i++) {
-			int n = sc.nextInt();
-			System.out.println(result[n]);
-		}
-	
-	}
+  public static final int MAX_SIZE = 100;
+  public static long[] result = new long[MAX_SIZE+1];
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+
+    result[1] = 1;
+    result[2] = 1;
+    result[3] = 1;
+
+    for(int i=4; i<=MAX_SIZE; i++){
+      result[i] = result[i-3] + result[i-2];
+    }
+
+    for (int i = 0; i < T; i++) {
+      int n = sc.nextInt();
+      System.out.println(result[n]);
+    }
+  }
+
 }
