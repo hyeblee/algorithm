9dc991202c4b5ffa88da335e194fef632a4df42a
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-06-10 16:53:28 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+  public static int N, M;
+  public static int[] arr;
+  public static int[] result;
+  public static StringBuilder sb = new StringBuilder("");
+
+  public static void dfs(int depth, int cur) {
+    if (depth == M) {
+      for (int n : result) {
+        sb.append(n).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+    for (int i = cur; i < N; i++) {
+      result[depth] = arr[i];
+      dfs(depth + 1, i + 1);
+    }
+  }
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    result = new int[M];
+    arr = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(arr);
+
+    dfs(0, 0);
+
+    System.out.println(sb.toString());
+  }
+
+}
