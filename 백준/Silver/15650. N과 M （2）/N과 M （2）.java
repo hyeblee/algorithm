f7307c44c9071d9d6ffe43ab1af21d1af7670c39
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-06-10 16:22:15 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,44 +1,40 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+import java.util.*;
+
+
 public class Main {
- 
-	public static int[] arr;
-	public static int N, M;
-	public static StringBuilder sb = new StringBuilder();
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
- 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
- 
-		arr = new int[M];
-        
-		dfs(1, 0);
-		System.out.println(sb);
- 
-	}
- 
-	public static void dfs(int at, int depth) {
- 
-		if (depth == M) {
-			for (int val : arr) {
-				sb.append(val).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
-        
-		for (int i = at; i <= N; i++) {
- 
-			arr[depth] = i;
-			dfs(i + 1, depth + 1);
- 
-		}
-	}
+  public static int N, M;
+  public static int[] result;
+  public static StringBuilder sb = new StringBuilder();
+
+  // 오름차순인 수열 만들기
+  public static void dfs(int depth, int cur) {
+    if (depth == M) {
+      for(int n: result) {
+        sb.append(n).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for(int i=cur;i<N;i++) {
+      result[depth] = i+1;
+      dfs(depth+1, i+1);
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    result = new int[M];
+
+    dfs(0, 0);
+    System.out.println(sb);
+  }
+
 }
