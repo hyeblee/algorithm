f430368747288841124f3497d729c952bdd72ac2
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-07-04 16:12:58 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1660 ms, Memory: 92380 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -4,27 +4,60 @@ import java.io.*;
 
 public class Main {
 
-  public static int[][][] dp = new int[21][21][21];
+  public static int MAX_SIZE = 50;
+  public static int[][][] dp = new int[2 * MAX_SIZE + 1][2 * MAX_SIZE + 1][2 * MAX_SIZE + 1];
 
-  public static void main(String[] args) throws IOException {
-    Scanner sc = new Scanner(System.in);
-
-    for (int a = 0; a < 21; a++) {
-      for (int b = 0; b < 21; b++) {
-        for (int c = 0; c < 21; c++) {
-          if (a <= 0 || b<= 0 || c<= 0) {
-            dp[a][b][c] = 1;
-          }
-          else if (a < b && b < c) {
-            dp[a][b][c] = dp[a][b][c - 1] + dp[a][b - 1][c - 1] - dp[a][b - 1][c];
+  public static void getDp() {
+    for (int a = -50; a <= 20; a++) {
+      for (int b = -50; b <= 20; b++) {
+        for (int c = -50; c <= 20; c++) {
+          int i = a + 50;
+          int j = b + 50;
+          int k = c + 50;
+          if (a <= 0 || b <= 0 || c <= 0) {
+            dp[i][j][k] = 1; // 1로 만들어주는 부분
+          } else if (a < b && b < c) {
+            dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k];
           } else {
-            dp[a][b][c] =
-                dp[a - 1][b][c] + dp[a - 1][b - 1][c] + dp[a - 1][b][c - 1] - dp[a - 1][b - 1][c
+            dp[i][j][k] =
+                dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k
                     - 1];
           }
         }
       }
     }
+
+    int tmp = dp[70][70][70];
+//    System.out.println(tmp);
+    for (int a = -50; a <= 50; a++) {
+      for (int b = -50; b <= 50; b++) {
+        for (int c = -50; c <= 50; c++) {
+          int i = a + 50;
+          int j = b + 50;
+          int k = c + 50;
+          if (a <= 0 || b <= 0 || c <= 0) {
+            dp[i][j][k] = 1; // 1로 만들어주는 부분
+          } else if (a > 20 || b > 20 || c > 20) {
+            dp[i][j][k] = tmp;
+            if (a==20 && b==20 && c==21){
+//              System.out.printf("%d %d %d - %d\n",a,b,c,dp[i][j][k]);
+            }
+          } else if (a < b && b < c) {
+            dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k];
+          } else {
+            dp[i][j][k] =
+                dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1]
+                    - dp[i - 1][j - 1][k - 1];
+          }
+        }
+      }
+    }
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    getDp();
 
     while (true) {
       int a = sc.nextInt();
@@ -34,15 +67,8 @@ public class Main {
       if (a == -1 && b == -1 && c == -1) {
         break;
       }
-      int result = 1;
-      if (a <= 0 || b <= 0 || c <= 0) {
-        result = 1;
-      } else if (a > 20 || b > 20 || c > 20) {
-        result = dp[20][20][20];
-      } else {
-        result = dp[a][b][c];
-      }
-      System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, result);
+
+      System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, dp[a + 50][b + 50][c + 50]);
     }
 
   }
