f1035ea2d7091c8f10a6173d97204d9606983fde
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-07-07 00:03:43 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 23916 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,26 +1,39 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
+
+  public static int n;
+  public static int[] arr;
+  public static int[] dp;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    int[] arr = new int[n];
+    n = Integer.parseInt(br.readLine());
+    arr = new int[n];
+    dp = new int[n];
 
     StringTokenizer st = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    int[] dp = new int[n];
+
+    // 이전 값에 현재값을 더하는 것이 나은지, 현재값만 쓰는게 나은지 판단
     dp[0] = arr[0];
     int max = dp[0];
-
-    for (int i = 1; i < n; i++) {
-      dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+    for(int i=1;i<n;i++) {
+      dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
       max = Math.max(max, dp[i]);
     }
-
+    // dp[n-1]은 이전 값 + 현재 vs 현재만 판단이라 정답이 아님.
     System.out.println(max);
+
+
+
+
   }
+
+
 }
