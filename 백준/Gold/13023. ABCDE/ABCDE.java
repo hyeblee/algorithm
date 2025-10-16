4c882ef46be80265568e0efb75a50b009d1ba8b0
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-10-16 22:37:22 +0900

    [Gold V] Title: ABCDE, Time: 420 ms, Memory: 37352 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static int n, m;
+    public static boolean[] visited;
+    public static List<Integer>[] link;    
+    public static int result = 0;
+
+    public static void dfs(int start, int depth) {
+        if (depth == 4) {
+            result = 1;
+            return;
+        }
+
+        visited[start] = true;
+        for(int next: link[start]) {
+            if (!visited[next]) {
+                dfs(next, depth + 1);
+            }
+        }
+        visited[start] = false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(new InputStreamReader(System.in));
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        visited = new boolean[n];
+        link = new ArrayList[n];
+        for(int i=0;i<n;i++) {
+            link[i] = new ArrayList<>();
+        }
+
+        for(int i=0;i<m;i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            link[u].add(v);
+            link[v].add(u);
+        }
+
+        for (int i=0;i<n;i++) {
+            dfs(i, 0);
+            if (result == 1)
+                break;
+        }
+
+        System.out.println(result);
+    }
+
+}
