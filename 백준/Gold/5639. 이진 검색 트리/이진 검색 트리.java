99fd4707af74d73a807fe8ee4072a638b0f1a3db
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-10-17 22:17:57 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 628 ms, Memory: 29220 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -3,45 +3,44 @@ import java.io.*;
 
 public class Main {
 
-    public static class Node {
-        int n;
-        Node l;
-        Node r;
+  public static class Node {
+    int n;
+    Node l;
+    Node r;
 
-        public Node(int n) {
-            this.n = n;
-        }
+    public Node(int n) {
+      this.n = n;
+    }
+  }
+
+  // BST 삽입
+  public static Node insert(Node root, int val) {
+    if (root == null) return new Node(val);
+
+    if (root.n < val) root.r =  insert(root.r, val);
+    else root.l = insert(root.l, val);
+    return root;
+  }
+
+  // 후위순회
+  public static void postOrder(Node root) {
+    if (root == null) return;
+
+     postOrder(root.l);
+     postOrder(root.r);
+     System.out.println(root.n);
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String line;
+    Node root = null;
+
+    while ((line = br.readLine()) != null && !line.isEmpty()) {
+      int val = Integer.parseInt(line);
+      root = insert(root, val);
     }
 
-    // BST 삽입
-    public static Node insert(Node root, int val) {
-        if (root == null) return new Node(val);
-
-        if (val < root.n) root.l = insert(root.l, val);
-        else root.r = insert(root.r, val);
-
-        return root;
-    }
-
-    // 후위순회
-    public static void postOrder(Node root) {
-        if (root == null) return;
-
-        postOrder(root.l);  // 왼쪽
-        postOrder(root.r);  // 오른쪽
-        System.out.println(root.n); // 루트
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        Node root = null;
-
-        while ((line = br.readLine()) != null && !line.isEmpty()) {
-            int val = Integer.parseInt(line);
-            root = insert(root, val);
-        }
-
-        postOrder(root);
-    }
+    postOrder(root);
+  }
 }
