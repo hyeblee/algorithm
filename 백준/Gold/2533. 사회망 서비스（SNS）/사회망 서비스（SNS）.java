88f895a2a8a1bfec88dac0c56d86d4f7e3934754
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-10-20 17:06:19 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2296 ms, Memory: 426572 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  // 정점개수 N
+  // N-1개의 간선 정보
+  // 부모가 얼리어답터면, 자식은 아니어도 됨
+  // 부모가 얼리어답터아니면, 자식은 무조건이어야함.
+
+  public static int n;
+  public static boolean[] visited;
+  public static List<Integer>[] graph;
+  public static int[][] dp;
+
+  public static void dfs(int depth) {
+    visited[depth] = true;
+    dp[depth][0] = 0;
+    dp[depth][1] = 1;
+
+    for (int child : graph[depth]) {
+      if (!visited[child]) {
+        dfs(child);
+        dp[depth][0] += dp[child][1]; // 부모가 아니면 자식은 무조건 얼리어답터
+        dp[depth][1] += Math.min(dp[child][0], dp[child][1]); // 부모가 얼리어답터면 자식은 상관없음
+      }
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    n = Integer.parseInt(br.readLine());
+    dp = new int[n + 1][2];
+    visited = new boolean[n + 1];
+    graph = new ArrayList[n + 1];
+    for (int i = 0; i < n + 1; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    StringTokenizer st;
+    for (int i = 0; i < n - 1; i++) {
+      st = new StringTokenizer(br.readLine());
+      int u = Integer.parseInt(st.nextToken());
+      int v = Integer.parseInt(st.nextToken());
+      graph[u].add(v);
+      graph[v].add(u);
+    }
+
+    dfs(1);
+    System.out.println(Math.min(dp[1][1], dp[1][0]));
+  }
+}
