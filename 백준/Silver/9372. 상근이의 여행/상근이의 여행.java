9a4bf13e8dd89d1e9c8abeeace030950a445fe6b
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-10-20 16:10:27 +0900

    [Silver IV] Title: 상근이의 여행, Time: 936 ms, Memory: 235380 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,18 +1,32 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt(); // 테스트 케이스 수
 
-        for (int tc = 0; tc < t; tc++) {
-            int n = sc.nextInt(); // 국가 수
-            int m = sc.nextInt(); // 비행기 종류
-            for (int i = 0; i < m; i++) {
-                sc.nextInt(); // a
-                sc.nextInt(); // b
-            }
-            System.out.println(n - 1); // 최소 비행 횟수 = 국가 수 - 1
-        }
+  // T
+  // 국가 수 N, 비행기 종류 M
+  // a,b 쌍 M 개
+  // 모든 정점을 잇는 간선 조합 찾기~
+
+
+  public static int t;
+  public static int n, m;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    t = sc.nextInt();
+    for (int i = 0; i < t; i++) {
+      n = sc.nextInt();
+      m = sc.nextInt();
+
+      for (int j = 0; j < m; j++) {
+        sc.nextInt();
+        sc.nextInt();
+      }
+
+      // 연결그래프에서 모든 정점을 잇는 최소 간선의 수는 항상 n-1개이다.
+      System.out.println(n - 1);
+
     }
+  }
 }
