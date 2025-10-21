b0e41f95606c7d8dbdf1e4700943b40d5a7fab0a
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-10-21 09:41:57 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 628 ms, Memory: 19364 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -3,44 +3,62 @@ import java.io.*;
 
 public class Main {
 
+  // 이진 검색 트리를 후위 순회한 결과를 출력하여라
   public static class Node {
+
     int n;
     Node l;
     Node r;
 
-    public Node(int n) {
+    public Node(int n, Node l, Node r) {
       this.n = n;
+      this.l = l;
+      this.r = r;
     }
   }
 
-  // BST 삽입
-  public static Node insert(Node root, int val) {
-    if (root == null) return new Node(val);
+  public static Node insert(Node root, int data) {
+    if (root == null) {
+      return new Node(data, null, null);
+    }
+    if (root.n > data) {
+      root.l = insert(root.l, data);
+    } else {
+      root.r = insert(root.r, data);
+    }
 
-    if (root.n < val) root.r =  insert(root.r, val);
-    else root.l = insert(root.l, val);
     return root;
   }
 
-  // 후위순회
-  public static void postOrder(Node root) {
-    if (root == null) return;
-
-     postOrder(root.l);
-     postOrder(root.r);
-     System.out.println(root.n);
+  // 후위순위는 왼오부 (자식 방문하고 부모 마지막)
+  public static void postorder(Node root) {
+    // 자식 없으면 출력하고 종료
+    if (root.l == null && root.r == null) {
+      System.out.println(root.n);
+      return;
+    }
+    if (root.l != null) {
+      postorder(root.l);
+    }
+    if (root.r != null) {
+      postorder(root.r);
+    }
+    System.out.println(root.n);
   }
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String line;
     Node root = null;
 
-    while ((line = br.readLine()) != null && !line.isEmpty()) {
-      int val = Integer.parseInt(line);
-      root = insert(root, val);
+    String line = br.readLine();
+
+    while (line != null && !line.isEmpty()) {
+      int n = Integer.parseInt(line);
+      root = insert(root, n);
+      line = br.readLine();
     }
 
-    postOrder(root);
+    postorder(root);
   }
+
 }
